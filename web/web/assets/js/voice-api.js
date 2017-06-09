364c6f148fa14041756967f131847ebb1f7e56a8
Maciej Mendrela <maciejmendrela@gmail.com>
2017-06-09 17:48:24 +0200

    recorder

---
+++ web/web/assets/js/voice-api.js
@@ -0,0 +1,71 @@
+var audio_context;
+var recorder;
+
+function __log(e, data) {
+  console.log(e);
+}
+
+function startUserMedia(stream) {
+  var input = audio_context.createMediaStreamSource(stream);
+  __log('Media stream created.');
+
+  recorder = new Recorder(input);
+  __log('Recorder initialised.');
+}
+
+function startRecording() {
+  recorder && recorder.record();
+  __log('Recording...');
+}
+
+function stopRecording() {
+  recorder && recorder.stop();
+  __log('Stopped recording.');
+  createDownloadLink();
+  recorder.clear();
+}
+
+function createDownloadLink() {
+  recorder && recorder.exportWAV(function(blob) {
+   var reader = new window.FileReader();
+   reader.readAsDataURL(blob);
+   reader.onloadend = function() {
+    convert(reader.result.split(',')[1]);
+  }
+});
+}
+
+var record = document.getElementById("record");
+
+record.onclick = function() {
+  if(record.getAttribute("data-status") == "1"){
+    startRecording();
+    record.style.background = "red";
+    record.style.color = "black";
+    record.setAttribute("data-status","2");
+  } else{
+    stopRecording();
+    record.style.background = "";
+    record.style.color = "";
+    record.setAttribute("data-status","1");
+  }
+
+}
+
+window.onload = function init() {
+  try {
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
+    window.URL = window.URL || window.webkitURL;
+
+    audio_context = new AudioContext;
+    __log('Audio context set up.');
+    __log('navigator.getUserMedia ' + (navigator.getUserMedia ? 'available.' : 'not present!'));
+  } catch (e) {
+    alert('No web audio support in this browser!');
+  }
+
+  navigator.getUserMedia({audio: true}, startUserMedia, function(e) {
+    __log('No live audio input: ' + e);
+  });
+};
